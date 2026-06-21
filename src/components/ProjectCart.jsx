2547df4c1bae7682f47a5a07ddf61d71c362269f
VinayakSharmaDev <vinayaksharma.dev@gmail.com>
2026-06-21 07:24:13 -0700

    add card

--- src/components/ProjectCart.jsx
+++ src/components/ProjectCart.jsx
@@ -11,7 +11,7 @@ function ProjectCard({ data }) {
                     <ul className="flex flex-row">
                         <li className="flex-1">{data.mode}</li>
                         <li><button className="bg-[#c75b12] mx-1 min-w-12 rounded-sm cursor-pointer hover:bg-amber-700 transition-colors duration-200"><a href={data.demoLink} target="_blank">Demo</a></button></li>
-                        <li><button className="bg-[#c75b12] min-w-12 rounded-sm cursor-pointer hover:bg-amber-700 transition-colors duration-200 "><a href={data.github} target="_blank">Live</a></button></li>
+                        <li><button className="bg-[#c75b12] min-w-12 rounded-sm cursor-pointer hover:bg-amber-700 transition-colors duration-200 "><a href={data.github} target="_blank">GitHub</a></button></li>
                     </ul>
                 </div>
             </div>
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,16 @@
+import portfolio from '/src/assets/img/portfolio.png';
+import spotify from '/src/assets/img/Minimal-Music-Player.png';
+import toDo from '/src/assets/img/Microsoft-To-Do.png';
+
 const projectsData = [
-    { title: 'Spotify - Home Page', discription: 'A minimal Spotify-inspired music player.', techStack: 'HTML, CSS, and JavaScript', image: '/src/assets/img/Minimal-Music-Player.png', mode: '🟢 Online', demoLink: 'https://vinayaksharma-minimal-music-player.netlify.app/', github: 'https://github.com/VinayakSharmaDev/minimal-music-player', id: crypto.randomUUID() },
-    { title: 'First Portfolio', discription: 'My first ever Portfolio', techStack: 'HTML, CSS, and JavaScript', image: '/src/assets/img/portfolio.png', mode: '🟢 Online', demoLink: 'https://vinayaksharmadev.netlify.app/', github: 'https://github.com/VinayakSharmaDev/VinayakSharmaDev', id: crypto.randomUUID() }
+    { title: 'Microsoft To Do', discription: 'Microsoft inspired to do with basic feature', techStack: 'React & Tailwind CSS', image: toDo , mode: '🟢 Online', demoLink: 'https://vinayaksharmadev.github.io/Microsoft-To-Do/', github: 'https://github.com/VinayakSharmaDev/Microsoft-To-Do/', id: crypto.randomUUID() },
+
+    { title: 'Spotify - Home Page', discription: 'A minimal Spotify-inspired music player.', techStack: 'HTML, CSS, and JavaScript', image: spotify, mode: '🟢 Online', demoLink: 'https://vinayaksharma-minimal-music-player.netlify.app/', github: 'https://github.com/VinayakSharmaDev/minimal-music-player', id: crypto.randomUUID() },
+
+    {
+        title: 'First Portfolio', discription: 'My first ever Portfolio', techStack: 'HTML, CSS, and JavaScript',
+        image: portfolio, mode: '🟢 Online', demoLink: 'https://vinayaksharmadev.netlify.app/', github: 'https://github.com/VinayakSharmaDev/VinayakSharmaDev', id: crypto.randomUUID()
+    }
 ]
 
 export default projectsData;
